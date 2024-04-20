a7feadf7e24028557ac634c43be0b11699d066d5
Kyla <kmtdacuba@gmail.com>
2024-04-20 23:36:39 +0800

    conniction

--- config/connection.php
+++ config/connection.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
     // Start session
@@ -13,18 +12,3 @@
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());// database connection
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
 ?>
-=======
- <?php
-// Start session
-     session_start();
-    // Create constant to store non repeating values
-+   define('SITEURL','https://brgymanagment.online/');
-+   define('LOCALHOST', 'localhost');
-+   define('DB_USERNAME', 'u783461092_khayeng19');
-+   define('DB_PASSWORD', 'Feb#192021');
-+   define('DB_NAME', 'u783461092_capstone');
-    // execure query and save data into databse
-     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());// database connection
-     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
- ?>
->>>>>>> 738c4c095b3e7ca70670c37975a6d2922832f44c
