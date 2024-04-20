bdb6a479dfe583a88ad84cfc0415e7c372f223de
Kmtdacuba <91925077+Kmtdacuba@users.noreply.github.com>
2024-04-20 21:47:14 +0800

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -6,7 +6,7 @@
    define('SITEURL','https://brgymanagment.online/');
    define('LOCALHOST', 'localhost');
    define('DB_USERNAME', 'u783461092_khayeng19');
-   define('DB_PASSWORD', '');
+   define('DB_PASSWORD', 'Feb#192021');
    define('DB_NAME', 'u783461092_capstone');
    // execure query and save data into databse
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());// database connection
