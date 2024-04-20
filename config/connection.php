dd254a2dd5eacc80fb650fd3bbf19bb3e99e4a9a
Kmtdacuba <91925077+Kmtdacuba@users.noreply.github.com>
2024-04-20 23:37:38 +0800

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,14 +1,13 @@
 <?php
-
-    // Start session
-    session_start();
-   // Create constant to store non repeating values
-   define('SITEURL','http://localhost/capstone//');
-   define('LOCALHOST', 'localhost: 3307');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_NAME', 'capstone');
-   // execure query and save data into databse
-    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());// database connection
-    $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
+// Start session
+     session_start();
+    // Create constant to store non repeating values
++   define('SITEURL','https://brgymanagment.online/');
++   define('LOCALHOST', 'localhost');
++   define('DB_USERNAME', 'u783461092_khayeng19');
++   define('DB_PASSWORD', 'Feb#192021');
++   define('DB_NAME', 'u783461092_capstone');
+    // execure query and save data into databse
+     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());// database connection
+     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
 ?>
