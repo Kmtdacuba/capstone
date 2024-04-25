bc3e947ff850ea13015cd05225af7a97c6fab382
Kyla <kmtdacuba@gmail.com>
2024-04-26 04:07:24 +0800

    replcae change pass to md5

--- change-pass.php
+++ change-pass.php
@@ -65,7 +65,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Verify if passwords match
     if ($new_password === $confirm_password1) {
         // Hash the new password
-        $hashed_password =  md5($new_password, PASSWORD_DEFAULT);
+        $hashed_password =  md5($new_password);
         // Update password in the database
         $email = $_SESSION['email'];
         $sql1 = "UPDATE tbl_admin SET password='$hashed_password' WHERE email='$email'";
