ca36d4e064afefbc6714dc50728680d26965e5ce
Kyla <kmtdacuba@gmail.com>
2024-04-26 19:36:47 +0800

    fixes for change-password.php

--- change-password.php
+++ change-password.php
@@ -1,6 +1,46 @@
 <?php
 include('config/connection.php');
+
+// Check if the form is submitted for password change
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $new_password = $_POST['new_password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    // Verify if passwords match
+    if ($new_password === $confirm_password) {
+        // Hash the new password
+        $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
+
+        // Update password in the respective table based on user type
+        if ($_SESSION['user_type'] == 'admin') {
+            $email = $_SESSION['email'];
+            $sql = "UPDATE tbl_admin SET password='$hashed_password' WHERE email='$email'";
+        } elseif ($_SESSION['user_type'] == 'employee') {
+            $email = $_SESSION['email'];
+            $sql = "UPDATE tbl_employee SET password='$hashed_password' WHERE email='$email'";
+        } elseif ($_SESSION['user_type'] == 'resident') {
+            $email = $_SESSION['email'];
+            $sql = "UPDATE tbl_resident SET password='$hashed_password' WHERE email='$email'";
+        }
+
+        // Execute the update query
+        if ($conn->query($sql) === TRUE) {
+            $_SESSION['change'] = "<div class='success text-center'>Password changed successfully!</div>";
+            header('location: index.php');
+            exit;
+        } else {
+            $_SESSION['change'] = "<div class='error text-center'>Error updating password: " . $conn->error . "</div>";
+            header('location: change-password.php');
+            exit;
+        }
+    } else {
+        $_SESSION['change'] = "<div class='error text-center'>Passwords do not match!</div>";
+        header('location: change-password.php');
+        exit;
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 
@@ -25,64 +65,20 @@ include('config/connection.php');
             </a>
             <h1>Change Password</h1>
             <?php
-                if(isset($_SESSION['change']))
-                {
-                    echo $_SESSION['change'];
-                    unset($_SESSION['change']);
-                }
-                if(isset($_SESSION['temp']))
-                {
-                    echo $_SESSION['temp'];
-                    unset($_SESSION['temp']);
-                }
-                if(isset($_SESSION['replace']))
-                {
-                    echo $_SESSION['replace'];
-                    unset($_SESSION['replace']);
-                }
-                ?>
+            if (isset($_SESSION['change'])) {
+                echo $_SESSION['change'];
+                unset($_SESSION['change']);
+            }
+            ?>
             <form method="post" action="change-password.php">
-                <tr>
-                    <input class="login-responsive" type="password" id="new_password" name="new_password"
-                        placeholder="Input New Password" required><br>
-                </tr>
-                <tr>
-                    <input class="login-responsive" type="password" id="confirm_password" name="confirm_password"
-                        placeholder="Confirm Your Password" required><br>
-                </tr>
-                <tr>
-                    <input class="btn-second" type="submit" value="Change Password">
-                </tr>
+                <input class="login-responsive" type="password" id="new_password" name="new_password"
+                    placeholder="Input New Password" required><br>
+                <input class="login-responsive" type="password" id="confirm_password" name="confirm_password"
+                    placeholder="Confirm Your Password" required><br>
+                <input class="btn-second" type="submit" value="Change Password">
             </form>
         </div>
     </center>
-
 </body>
 
 </html>
-<?php
-
-/* FOR ADMIN */
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $new_password = $_POST['new_password'];
-    $confirm_password = $_POST['confirm_password'];
-
-    // Verify if passwords match
-    if ($new_password === $confirm_password) {
-        // Hash the new password
-        $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
-        // Update password in the database
-        $email = $_SESSION['email'];
-        $sql = "UPDATE tbl_admin SET password='$hashed_password' WHERE email='$email'";
-
-        // Database connection and query execution code here...
-        $_SESSION['change'] = " <div class='success text-center'>Password changed successfully!</div>";
-        header('location: index.php');
-    } else {
-        $_SESSION['change'] = " <div class='success text-center'>Passwords do not match!</div>";
-        header('location: change-password.php');
-    }
-}
-
-
-?>
