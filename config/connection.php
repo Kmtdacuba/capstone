03a9fe0ea28bf1b6e0759d3d8a793c155b3497a1
Kyla <kmtdacuba@gmail.com>
2024-04-26 18:47:09 +0800

    finalized form.php

--- config/connection.php
+++ config/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+
 
 // Start session
     session_start();
@@ -13,7 +13,7 @@
    // execure query and save data into databse
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());// database connection
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
-*/
+/*
 
 // Start session
      session_start();
@@ -27,5 +27,5 @@
         // execure query and save data into databse
      $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());// database connection
      $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
-
+*/
 ?>
