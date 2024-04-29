d88dedb4277a177b6460305443739f0561c81b8e
Kyla <kmtdacuba@gmail.com>
2024-04-29 16:34:58 +0800

    public connection

--- config/connection.php
+++ config/connection.php
@@ -26,5 +26,4 @@
      $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());// database connection
      $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
 
-  x
 ?>
