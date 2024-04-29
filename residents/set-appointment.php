db776f472777ee5656a575482082e9a342919592
Kyla <kmtdacuba@gmail.com>
2024-04-29 16:25:08 +0800

    fix redirection going to summary.php

--- residents/set-appointment.php
+++ residents/set-appointment.php
@@ -277,10 +277,10 @@ if (isset($_POST["selected_time"])) {
     if (mail($to, $subject, $message, $headers)) {
  
         $_SESSION['sent'] = " <div class='success'> &nbsp;Scheduled appointment details sent to your email</div>";
-           header('location:'. SITEURL.'residents/my-appointment.php');
+          
     } else {
         $_SESSION['sent'] = " <div class='error'> &nbsp;Unseccessful to send appointment details</div>";
-        header('location:'. SITEURL.'residents/set-appointment.php');
+
      } 
 // EXECUTE QUERY AND SAVE DATA IN DATABASE
 $res = mysqli_query($conn, $sql) or die(mysqli_error());
@@ -290,7 +290,7 @@ $res = mysqli_query($conn, $sql) or die(mysqli_error());
 if($res == TRUE){
 // data inserted
 // variable to display message;
-$_SESSION['email '] = $eamil;
+$_SESSION['email '] = $email;
 $_SESSION['appointment']="<div class='success text-center'> &nbsp;Appointment set successfully</div>";
 header("Location:".SITEURL.'residents/summary.php');
 exit();
