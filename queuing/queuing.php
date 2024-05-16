202e6864de1078fffe51af85e8180b677deeafa2
Kyla <kmtdacuba@gmail.com>
2024-05-16 19:02:52 +0800

    fix counter_no in tbl_queuing

--- queuing/queuing.php
+++ queuing/queuing.php
@@ -115,35 +115,9 @@ function limitInput(field) {
 </html>
 <?php 
 
-// Function to get the next counter
-function getNextCounter($conn) {
-    $query = "SELECT MAX(counter_no) AS max_counter FROM tbl_queuing";
-    $result = $conn->query($query);
-    $row = $result->fetch_assoc();
-    $max_counter = $row['max_counter'];
-
-    // If there are no numbers in the database, return 1
-    if ($max_counter === null) {
-        return 1;
-    } else {
-        // Calculate the next counter
-        $next_counter = ($max_counter % 5) + 1;
-
-        // Reset counter to 1 if it reaches 5
-        if ($next_counter == 5) {
-            $reset_query = "UPDATE tbl_queuing SET counter_no = 1";
-            $conn->query($reset_query);
-        }
-
-        return $next_counter;
-    }
-}
-
-    if(isset($_POST['submit'])) {
-        
-        $counter_no = $_POST['counter_no'];
-        $appointment_no = $_POST['appointment_no'];
-        $date_time = date("Y-m-d h:i:sa"); //time and date
+if(isset($_POST['submit'])) {
+    $appointment_no = $_POST['appointment_no'];
+    $date_time = date("Y-m-d h:i:sa"); //time and date
 
     // Check if appointment number already exists
     $sql_check = "SELECT * FROM tbl_queuing WHERE appointment_no='$appointment_no'";
@@ -154,54 +128,57 @@ function getNextCounter($conn) {
         header("location:" .SITEURL.'queuing/queuing.php');
         exit();
     }
-// Check if appointment number exists in table appointment
-$sql_number = "SELECT * FROM tbl_appointment WHERE appointment_no='$appointment_no'";
-$res_number = mysqli_query($conn, $sql_number) or die(mysqli_error($conn));
 
-if (mysqli_num_rows($res_number) == 0) {
-    $_SESSION['number'] = "<div class='error'>Appointment number doesn't exist in table</div>";
-    header("Location: queuing.php");
-    exit();
-}
-    
-    $sql1 = "SELECT * FROM tbl_appointment";
-    $res1 = mysqli_query($conn, $sql1) or die(mysqli_error);
+    // Check if appointment number exists in table appointment
+    $sql_number = "SELECT * FROM tbl_appointment WHERE appointment_no='$appointment_no'";
+    $res_number = mysqli_query($conn, $sql_number) or die(mysqli_error($conn));
+
+    if (mysqli_num_rows($res_number) == 0) {
+        $_SESSION['number'] = "<div class='error'>Appointment number doesn't exist in table</div>";
+        header("Location: queuing.php");
+        exit();
+    }
+
     $bday = new Datetime(date('Y-m-d', strtotime($_POST['Birthday']))); // Creating a DateTime object representing your date of birth.
     $today = new Datetime(date('Y-m-d')); // Creating a DateTime object representing today's date.
     $diff = $today->diff($bday); 
 
-    $sql_age = "SELECT * FROM tbl_appointment";
-    
-        // Sql query to serve the data into database
-        $sql = "INSERT INTO tbl_queuing SET 
-        queue_no = '$new_queue_no',
-        counter_no = '$counter_no',
-        age='$diff->y',
-        appointment_no = '$appointment_no',
-        date_time = '$date_time'
-        ";
-        
-        
-        // EXECUTE QUERY AND SAVE DATA IN DATABASE
-       $res = mysqli_query($conn, $sql) or die(mysqli_error());
+    // Get the last counter_no
+    $sql_last_counter = "SELECT counter_no FROM tbl_queuing ORDER BY queue_no DESC LIMIT 1";
+    $res_last_counter = mysqli_query($conn, $sql_last_counter) or die(mysqli_error($conn));
 
-       // check if data is inserted or not and display message;
+    if (mysqli_num_rows($res_last_counter) > 0) {
+        $row_last_counter = mysqli_fetch_assoc($res_last_counter);
+        $last_counter_no = $row_last_counter['counter_no'];
+        $counter_no = ($last_counter_no % 5) + 1;
+    } else {
+        $counter_no = 1; // Default to 1 if there are no previous entries
+    }
 
-       if($res == TRUE) {
+    // Sql query to serve the data into database
+    $sql = "INSERT INTO tbl_queuing SET 
+    counter_no = '$counter_no',
+    age = '{$diff->y}',
+    appointment_no = '$appointment_no',
+    date_time = '$date_time'
+    ";
+
+    // EXECUTE QUERY AND SAVE DATA IN DATABASE
+    $res = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+    // check if data is inserted or not and display message
+    if($res == TRUE) {
         // data inserted
         // variable to display message;
-        $_SESSION['queuing_id'] = $row['id'];
-        $_SESSION['wait'] = " <div class='success text-center'Please wait for your turn./div>";
-        header("location:" .SITEURL.'queuing/queue-num.php');
-        
-        exit;
-        
-       }
-       else {
+        $_SESSION['queuing_id'] = mysqli_insert_id($conn);
+        $_SESSION['wait'] = "<div class='success text-center'>Please wait for your turn.</div>";
+        header("location:" . SITEURL . 'queuing/queue-num.php');
+        exit();
+    } else {
         // data not inserted
-        $_SESSION['wait'] = " <div class='success text-center'>Error occured, please try again</div>";
-        header("location:" .SITEURL.'queuing/queue-num.php');
-        exit;
-       }
-    } 
+        $_SESSION['wait'] = "<div class='error text-center'>Error occurred, please try again</div>";
+        header("location:" . SITEURL . 'queuing/queue-num.php');
+        exit();
+    }
+}
 ?>
